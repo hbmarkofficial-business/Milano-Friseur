b92fde0d75d442ebd484055231a416187477f361
hbmarkofficial-business <hbmark.official@gmail.com>
2026-01-27 14:56:47 +0100

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -131,14 +131,14 @@ const Contact = () => {
 
                   <div className="aspect-video rounded-sm overflow-hidden border border-border">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2593.0466369434594!2d11.117062376893147!3d49.464733457845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f57dec87eba7f%3A0xac674ba124944238!2sLord%20Barbershop%20Coiffeur!5e0!3m2!1sde!2sde!4v1769174581720!5m2!1sde!2sde"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10377.620897423014!2d11.032417187158204!3d49.4390577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f57e38ff71877%3A0x508728322859f8a7!2sMilano%20Friseur!5e0!3m2!1sde!2sde!4v1769522495584!5m2!1sde!2sde"
                       width="100%"
                       height="100%"
                       style={{ border: 0, filter: 'grayscale(100%) contrast(1.2)' }}
                       allowFullScreen
                       loading="lazy"
                       referrerPolicy="no-referrer-when-downgrade"
-                      title="Standort Lord Barbershop Coiffeur"
+                      title="Standort Milano Friseur"
                     />
                   </div>
                 </div>
@@ -154,6 +154,5 @@ const Contact = () => {
 };
 
 export default Contact;
-
 
 
